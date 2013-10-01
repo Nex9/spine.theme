e3a372f972c6ae5c06a57fb9007128fb1bb6fe25
Christof Haemmerle <reco@nex9.com>
2013-09-30 21:18:18 -0400

    more stuff

---
+++ app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import os, re, logging
+
+from base import decorators
+from base.handlers import BaseHandler, StaticFileHandler
+from base.models import CacheFlushCounter
+
+# custom app imports
+from imageapp import models
+
+
+from appengine_config import DEBUG
+
+
+
+class Main(BaseHandler):
+    @decorators.normalize_url
+    @decorators.cache_page(with_keywords=True)
+    def get(self, path):
+        if not path: path = '/'
+        assetlookup = models.AssetLookup.by_path(path)
+        if assetlookup is None:
+            return self.status(404, 'Collection Not Found')
+
+        page = assetlookup.assetkey.get()
+        self.render('index.html', page = page)
+
+
+
+settings = {
+    "template_path": os.path.join(os.path.dirname(__file__), "public"),
+    "static_path": os.path.join(os.path.dirname(__file__), "public"),
+    "xsrf_cookies": True,
+    "debug": DEBUG,
+    "cacheflushcounter" : CacheFlushCounter.get_count()
+}
+
+routes = [
+    (r"/(.*\.\w{2,4}$)/?", StaticFileHandler),
+    (r"/(.*)/?", Main)
+]
---
+++ app/lib/setup.coffee
@@ -0,0 +1,21 @@
+require('json2ify')
+require('es5-shimify')
+require('jqueryify')
+require('underscore')
+
+require('spine')
+require('spine/lib/ajax')
+require('spine/lib/manager')
+require('spine/lib/route')
+require('lib/touch')
+
+# require('lib/screenfull')
+
+require('nex')
+require('nex/lib/utils')
+require('nex/lib/models')
+require('nex/lib/panel')
+require('nex/lib/widgets')
+require('nex/lib/slider')
+require('nex/lib/html')
+
