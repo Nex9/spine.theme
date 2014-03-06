a2a893818c03c3a928ffb7b26999d0eae96391c4
Andreas Schmid <andreas@nex9.com>
2014-03-06 14:34:08 -0500

    updated online.html and app.py

--- app.py
+++ app.py
@@ -15,16 +15,40 @@ from appengine_config import DEBUG
 
 
 class Main(BaseHandler):
+
+    def pagelinks(self, next=None, prev=None):
+        if not next and not prev:
+            return None, None
+        rgx = re.search(r'/page/(\d+)', self.path)
+        num = int(rgx.group(1)) if rgx else 1
+        if num == 1 and not rgx:
+            nextlink = self.path.rstrip('/') + '/page/2'
+        else:
+            nextlink = self.path.replace(rgx.group(), '/page/%s'%(num+1)) if next else None
+        prevlink = self.path.replace(rgx.group(), '/page/%s'%(num-1)) if prev else None
+        return nextlink, prevlink
+
+    def get_assets(self, *args, **kwargs):
+        return models.Collection.get_assets(*args, **kwargs)
+
+
+    def pagenum(self, pagenum, page):
+        if self.imago.get(page, 'pagesize', 0):
+            return 1 if not pagenum else int(pagenum)
+        return None
+
+
     @decorators.normalize_url
     @decorators.cache_page(with_keywords=True)
-    def get(self, path):
+    def get(self, path, pagenum=None):
         if not path: path = '/'
         assetlookup = models.AssetLookup.by_path(path)
         if assetlookup is None:
             return self.status(404, 'Collection Not Found')
 
-        page = assetlookup.assetkey.get()
-        self.render('online.html', page = page)
+        page    = assetlookup.assetkey.get()
+
+        self.render('index.html', page=page, pagenum=self.pagenum(pagenum, page))
 
 
 
@@ -38,5 +62,7 @@ settings = {
 
 routes = [
     (r"/(.*\.\w{2,4}$)/?", StaticFileHandler),
+    (r"/(.*)/page/([0-9]+)/?", Main),
     (r"/(.*)/?", Main)
+
 ]
